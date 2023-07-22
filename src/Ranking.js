51a1623a9e83aa0102bb8d0bbc1716349d6ffc9e
k-e-n-5 <kengo.afternoon.tea@gmail.com>
2023-07-22 05:32:46 +0000

    レイアウト調整

--- src/Ranking.js
+++ src/Ranking.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from "react";
+import "./Ranking.css";
+import logo_js from "./js_logo_symbol.jpg"
 
 function Ranking() {
 
     const [dbData, setDbData] = useState([]);
+    let count = 1;
 
     useEffect(() => {
         fetch('/dbTest')
@@ -11,15 +14,41 @@ function Ranking() {
     },[])
 
     return (
-        <div>
-            <label>TEST</label>
-            
+        <div className="style_a">
+            <h1>
+                <label>Japan System</label>
+                <br></br>
+                <label>JS Token Ranking!</label>
+            </h1>
             {/*JSON形式で受け取ったオブジェクトを表示させる方法*/}
-	        <ul>{dbData.map((data) => (
-	            <li key={data.postUser} style={{listStyleType:"none"}}>
-		            {data.postUser} {data.likeCount}
-	            </li>
-            ))}</ul>
+
+            <div className="style_b">
+                <table>
+                    <tbody>
+                    <tr class="h_table">  
+                        <th>Rank</th>
+                        <th>Name</th>
+                        <th>Token</th>
+                    </tr >
+                    {dbData.map((data) => (
+                    <tr key={data.postUser} style={{listStyleType:"none"}} >
+                    <td>{count++}</td><td>{data.postUser}</td><td>{data.likeCount}</td>
+                    </tr>))}
+
+
+                    <tr>
+                        
+                    </tr>
+
+
+
+
+                </tbody>
+            </table>
+            </div>
+            <br></br>
+            <img src={logo_js} alt="logo_js" width="150px" />
+
         </div>
     );
 }
